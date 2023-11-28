7bc38caf04597c415927515c4d6001409a5e2f76
Rohit Sure <rohitsure.9915@gmail.com>
2023-11-29 00:56:29 +0530

    "characters & messages file added"

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className='generator'>
+          <h2 className='generator_header'>
+            Password Generator
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
