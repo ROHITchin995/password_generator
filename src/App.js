82ef37e5e9aef549866f83a1ca2281b4476be043
Rohit Sure <rohitsure.9915@gmail.com>
2023-11-29 14:56:24 +0530

    notify, copy & generate password function added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,55 @@ const App = () => {
   const [includeNumbers, setIncludeNumbers] = useState(false)
   const [includeSymbols, setIncludeSymbols] = useState(false)
 
+  const notify = (message, hasError = false)=>{
+    if(hasError){
+      toast.error(message,{
+
+      })
+    }
+    else{
+      toast(message, {
+
+      })
+    }
+  }
+
+  const copyToClipboard = (password) =>{
+    navigator.clipboard.writeText(password)
+  }
+
+  const handleGeneratePassword = () => {
+    if(!includeLowerCase && !includeUpperCase && !includeNumbers && includeSymbols){
+      notify("To generate password you must select atleast one checkbox", true)
+    }
+    else{
+      let characterList = ""
+      if(includeNumbers){
+        characterList = characterList + numbers
+      }
+      if(includeUpperCase){
+        characterList = characterList + upperCaseLetters
+      }
+      if(includeLowerCase){
+        characterList = characterList + lowerCaseLetters
+      }
+      if(includeSymbols){
+        characterList = characterList + specialCharacters
+      }
+      setPassword(createPassword(characterList))
+      notify("Password is generated successfully", false)
+    }
+  }
+
+  const handleCopyPassword = (e)=>{
+    if (password === ""){
+      notify(COPY_Fail, true)
+    }
+    else{
+      copyToClipboard(password)
+      notify(COPY_SUCCESS)
+    }
+  }
   return (
     <div className="App">
       <div className='container'>
@@ -23,7 +72,7 @@ const App = () => {
           <div className='generator_password'>
             <h3>{password}</h3>
             <button className='copy_btn'>
-              <i className='far fa-clipboard'></i>
+              <i onClick={handleCopyPassword} className='far fa-clipboard'></i>
             </button>
           </div>
           <div className='form-group'>
@@ -46,7 +95,7 @@ const App = () => {
             <label htmlFor="include-symbols">Include Symbols</label>
             <input checked={includeSymbols} onChange={(e) => setIncludeSymbols(e.target.checked)} type="checkbox" id="include-symbols" name="include-symbols" />
           </div>
-          <button>
+          <button onClick={handleGeneratePassword}>
             Generate Password
           </button>
           <ToastContainer />
