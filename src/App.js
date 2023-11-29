7cd658379f3012ef121d919334fc449438d2897e
Rohit Sure <rohitsure.9915@gmail.com>
2023-11-29 15:03:55 +0530

    notify function completed

--- src/App.js
+++ src/App.js
@@ -16,12 +16,24 @@ const App = () => {
   const notify = (message, hasError = false)=>{
     if(hasError){
       toast.error(message,{
-
+        position:"top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
       })
     }
     else{
       toast(message, {
-
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
       })
     }
   }
